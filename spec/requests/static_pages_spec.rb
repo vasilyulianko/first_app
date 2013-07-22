ed16d65ca7d790097b395db4afaa8694f76cb64b
Vasily Ulianko <vasily.ulianko@gmail.com>
2013-07-22 22:33:44 +0100

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,14 +7,28 @@ feature "Static pages" do
       expect(page).to have_content('Sample App')
   end
 
+  scenario "User opens Home page, should have the right title" do
+    visit '/static_pages/home'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+  end
+
   scenario "User opens Help page, 'Help' text should present" do
     visit '/static_pages/help'
       expect(page).to have_content('Help')
   end
 
+  scenario "User opens Help page, should have the right title" do
+    visit '/static_pages/help'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+  end
 
-  scenario "User opens About, 'About Us' text should present" do
+  scenario "User opens About page, 'About Us' text should present" do
     visit '/static_pages/about'
       expect(page).to have_content('About Us')
+  end
+
+  scenario "User opens About page, should have the right title" do
+    visit '/static_pages/about'
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
   end
 end

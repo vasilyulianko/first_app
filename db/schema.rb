441b9df5893e35abcb7f0a26f8b68d187d3944f8
Vasily Ulianko <vasily.ulianko@gmail.com>
2013-07-28 19:38:55 +0100

    Add password

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130727140807) do
+ActiveRecord::Schema.define(version: 20130728181811) do
 
   create_table "microposts", force: true do |t|
-    t.string   "content"
-    t.integer  "user_id"
+    t.string "content"
+    t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
+    t.string "name"
+    t.string "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
